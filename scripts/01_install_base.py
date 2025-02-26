3ffba618f5d1590af71a9bdedcd7fcaecfb2cd5b
Dhananjay Lokhande <125077594+Dhananjay-97@users.noreply.github.com>
2025-02-26 18:04:36 +0530

    Update 01_install_base.py

--- scripts/01_install_base.py
+++ scripts/01_install_base.py
@@ -6,9 +6,3 @@ os.chdir(root_dir)
 
 print(subprocess.run(["python scripts/validator/validate_env.py"], shell=True, check=True))
 
-print(
-    subprocess.run(
-        ["bash scripts/install_qdrant.sh"], shell=True, check=True
-    )
-)
-print("Installing Qdrant is complete")
